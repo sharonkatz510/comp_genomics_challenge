9934d224944672869979e3b34da9808db8339d8c
Idan Lavi <idaneurosc@gmail.com>
2020-06-07 10:47:31 +0300

    .

--- feature_selection.py
+++ feature_selection.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from util import *
 
 
 def spear_corr(x, y):
